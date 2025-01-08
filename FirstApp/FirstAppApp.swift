08392e6527ab037d34a4e516244051038299b070
Roberto Konanz <robkonanz@gmail.com>
2025-01-07 19:58:12 -0800

    Initial Commit

---
+++ FirstApp/FirstAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstAppApp.swift
+//  FirstApp
+//
+//  Created by Roberto Konanz on 1/7/25.
+//
+
+import SwiftUI
+
+@main
+struct FirstAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FirstAppTests/FirstAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  FirstAppTests.swift
+//  FirstAppTests
+//
+//  Created by Roberto Konanz on 1/7/25.
+//
+
+import Testing
+@testable import FirstApp
+
+struct FirstAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
